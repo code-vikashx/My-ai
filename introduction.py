392afcea5f552313a334d4e2faea3f43d9e2e6cb
code-vikashx <vikashkumarbikash947@gmail.com>
2025-10-12 10:07:44 +0530

    Add files via upload
    It's my first project or program please give me star for me motivate

---
+++ introduction.py
@@ -0,0 +1,12 @@
+name=input("name:")
+print("hello welcome" , name+"*")
+age=int(input("age:"))
+if age <24:
+  print("hai bro welcome in this site!")
+elif age<40:
+  print("welcome sir in this programm")
+else:
+  	print("hello sir may i help you sir in this programm")
+choice=input("yes/no?=")
+plan=input("what's your plan sir for today:")
+print("how can i help you sir")
